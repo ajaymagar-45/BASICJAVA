97ed91b3a8ed2da50be9719e5fc7c2c09e291890
AJAY <ajaymagar812@gmail.com>
2025-09-18 13:20:53 +0530

    Added New Commit

--- src/Array/Arrays_1.java
+++ src/Array/Arrays_1.java
@@ -2,26 +2,26 @@ package Array;
 
 public class Arrays_1{
 public static void main(String[] args){
-    char[] roll_no={};
-    int [] a={};
-    char [] c={};
-    double [] d={};
-    float[]f={};
-    String [] s={"a"};
-    System.out.println(roll_no);
-    System.out.println(a);
-    System.out.println(c);
-    System.out.println(d);
-    System.out.println(f);
-    System.out.println(s[0]);
+//    char[] roll_no={};
+//    int [] a={};
+    char [] c=new char [3];
+    char newc[]=new char[c.length];
+//    double [] d={};
+//    float[]f={};
+//    String [] s={"a"};
+//    System.out.println(roll_no);
+//    System.out.println(a);
+//    System.out.println(c);
+//    System.out.println(d);
+//    System.out.println(f);
+//    System.out.println(s[0]);
+    for(int no:newc){
+        System.out.println(no);
+    }
 
 
-
+//kvlcmxl
 
 
     }
 }
-//
-
-//
-//mmn nm
