2fb5bec0bd22d7b22db7c87adf19625f6be88b84
ajaymagar_45 <ajaymagar812@gmail.com>
2025-10-12 14:40:59 +0530

    new

---
+++ src/Local.java
@@ -0,0 +1,12 @@
+public class Local {
+    void local(){
+        int a;
+        a=20;
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        Local o=new Local();
+        o.local();
+    }
+}
