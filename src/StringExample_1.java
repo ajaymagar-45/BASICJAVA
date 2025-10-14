3fb1d650c9f60b07946d7d08227bd0693cd4c297
ajaymagar_45 <ajaymagar812@gmail.com>
2025-10-14 15:26:45 +0530

    Example of String

---
+++ src/StringExample_1.java
@@ -0,0 +1,25 @@
+public class StringExample_1 {
+    String name="Ajay";
+
+//    @Override
+//    public String toString() {
+//        return (name);
+//    }
+
+
+    public static void main(String[] args) {
+
+             String a1$="Ajay";
+            String a2="Magar";
+            a1$=a2;
+
+        System.out.println(a1$);
+        System.out.println(a2);
+
+
+
+//        StringExample_1 obj=new StringExample_1();
+//        System.out.println(obj.toString());
+//        System.out.println(obj.hashCode());
+    }
+}
