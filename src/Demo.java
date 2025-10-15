dbfd06872199e867dc19969ca2535bf6b0bc2179
ajaymagar_45 <ajaymagar812@gmail.com>
2025-10-15 11:54:06 +0530

    other changes

--- src/Demo.java
+++ src/Demo.java
@@ -1,13 +1,13 @@
 public class Demo {
-    void show(int a, long b){
+    void show(int a, double b){
         System.out.println("short");
     }
-    void show(long a,int b){
+    void show(double a,int b){
         System.out.println("int");
     }
     public static void main(String[] args) {
         Demo o=new Demo();
-        o.show(4,5l);
+        o.show(4d,5);
 //
 
     }
