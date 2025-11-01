eaccfd6f5c2cf7f5e62ddca4dd3347b10e574134
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-01 13:08:01 +0530

    New code added

---
+++ src/JavaBasics/StingExa_2.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class StingExa_2 {
+    public static void main(String[] args) {
+        String s1="First_Name ";
+        String s3="Last_Name";
+        String s2=s1.concat(s3);
+        System.out.println(s2);
+    }
+}
--- src/StringExample_1.java
+++ src/StringExample_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class StringExample_1 {
-    String name="Ajay";
+//    String name="Ajay";
 
 //    @Override
 //    public String toString() {
@@ -9,10 +9,12 @@ public class StringExample_1 {
 
 
     public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name=obj.nextLine();
-
+//        Scanner obj=new Scanner(System.in);
+//        System.out.println("Enter your name: ");
+//        String name=obj.nextLine();
+           String s1="A";
+           String s2=s1+"B";
+        System.out.println(s2);
 
 //             String a1$="Ajay";
 //            String a2="Magar";
