5fcf6698656d5f207ea041cf971d011d162f8b59
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-04 16:55:41 +0530

    New List folder is Created

---
+++ src/List/L1.java
@@ -0,0 +1,20 @@
+package List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L1 {
+    public static void main(String [] args){
+        List<String> f=new ArrayList<>();
+        f.add("Hello");
+        f.add("World");
+        f.add("Java ");
+        f.add("Programming");
+        System.out.println(f);
+        List<Integer> ele=new ArrayList<>();
+        ele.add(10);
+        ele.add(20);
+        ele.add(30);
+        System.out.println(ele);
+    }
+}
