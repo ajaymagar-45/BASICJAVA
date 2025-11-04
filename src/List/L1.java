de48305263e730c45167b43aa057a19856c50b67
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-04 17:00:53 +0530

    Some changes made

--- src/List/L1.java
+++ src/List/L1.java
@@ -10,11 +10,18 @@ public class L1 {
         f.add("World");
         f.add("Java ");
         f.add("Programming");
-        System.out.println(f);
+        System.out.println(f.get(1));
+        System.out.println("   ");
+        for(String name:f){
+            System.out.println(name);
+        }
+        System.out.println("    ");
         List<Integer> ele=new ArrayList<>();
         ele.add(10);
         ele.add(20);
         ele.add(30);
         System.out.println(ele);
+        System.out.println(ele.get(2));
+        System.out.println("   ");
     }
 }
