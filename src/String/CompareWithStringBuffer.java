d4b0a51d6547f15e9df58780ca62e93e22644b1d
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-05 15:23:07 +0530

    _

--- src/String/CompareWithStringBuffer.java
+++ src/String/CompareWithStringBuffer.java
@@ -9,6 +9,7 @@ public class CompareWithStringBuffer {
 
             // Create a StringBuffer object strbuf initialized with the value of str1.
             StringBuffer strbuf = new StringBuffer(str1);
+            //
 
             // Compare str1 and strbuf for content equality and print the result.
             System.out.println("Comparing " + str1 + " and " + strbuf + ": " + str1.contentEquals(strbuf));
