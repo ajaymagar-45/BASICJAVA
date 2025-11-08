24a14f8387faa83d7f0f2bc1f8b89516d7e4f064
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-08 14:35:05 +0530

    ok

--- src/String/CaseInsensitive.java
+++ src/String/CaseInsensitive.java
@@ -3,14 +3,23 @@ package String;
 public class CaseInsensitive {
 
         public static void main(String[] args) {
-            String s1 = "Hello";
-            String s2 = "hello";
+//            String s1 = "Hello";
+//            String s2 = "hello";
+//
+//            if (s1.equalsIgnoreCase(s2)) {
+//                System.out.println("Strings are equal (ignoring case)");
+//            } else {
+//                System.out.println("Strings are NOT equal");
+//            }
 
-            if (s1.equalsIgnoreCase(s2)) {
-                System.out.println("Strings are equal (ignoring case)");
-            } else {
-                System.out.println("Strings are NOT equal");
-            }
+            String a="AJAY";
+            String b="ajay";
+            if(a.equalsIgnoreCase(b))
+                System.out.println(" given Strings are same in ignoring the case");
+
+            else
+                System.out.println("Given strings are not equal");
+
         }
     }
 
