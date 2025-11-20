ea7abd22d5c52113606d9e83aab6f456216107f9
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-20 14:03:02 +0530

    20-11-2025

---
+++ src/GetBinaryDigits.java
@@ -0,0 +1,11 @@
+public class GetBinaryDigits {
+    public static void main(String[] args) {
+        int a=176387;
+        int b=73467;
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+
+        System.out.println(a>b ? a:b);
+
+    }
+}
