468db23b3d67fe01239aef27a441229976fe7316
ajaymagar_45 <ajaymagar812@gmail.com>
2025-11-27 16:40:47 +0530

    27-11-2025

--- src/Local.java
+++ src/Local.java
@@ -1,12 +1,11 @@
 public class Local {
-    void local(){
-        int a;
-        a=20;
-        System.out.println(a);
+    static {
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
     }
 
     public static void main(String[] args) {
-        Local o=new Local();
-        o.local();
+
     }
 }
