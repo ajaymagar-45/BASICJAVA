426a53fa884ad8c88e80e674932978665061bb20
ajaymagar_45 <ajaymagar812@gmail.com>
2025-12-02 17:04:41 +0530

    02-12-2025

---
+++ src/Logic/PositiveNegativecheck.java
@@ -0,0 +1,11 @@
+package Logic;
+
+public class PositiveNegativecheck {
+    public static void main(String[] args) {
+        int n=-38;
+        if(n>0)
+            System.out.println(n + " is a positive number ");
+        else
+            System.out.println(n + " is a negative number");
+    }
+}
